d8e404aa388605f8d2397fe2936c4ec84953001c
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-08 11:43:48 +0530

    created a express server

--- npm/index.js
+++ npm/index.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+import express from "express"
+
+const app = express()
+app.get(
+    "/",()=>{
+        console.log("hello world")
+    }
+)
+
+app.listen(8000,()=>{
+    console.log("server running on port 8000")
+})
