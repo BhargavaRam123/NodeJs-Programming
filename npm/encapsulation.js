11d3876997bceaac9cc5e0521430df3ccf03e66a
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-08 15:45:56 +0530

    feat: implement private properties in person2 class with example usage

--- npm/encapsulation.js
+++ npm/encapsulation.js
@@ -13,3 +13,22 @@ class person {
 }
 const person1 = new person("John", 30);
 console.log(person1.name); 
+
+
+//implementing private properties in objects
+
+class person2{
+    #a
+    b
+    constructor(a,b){
+        this.#a = a
+        this.b = b
+    }
+    func = ()=>{
+        console.log("values are",this.#a,this.b)
+    }
+}
+
+const obj2 = new person2(2,3)
+// console.log(obj2.#a) //not accessible outside the class because it is private
+obj2.func()
