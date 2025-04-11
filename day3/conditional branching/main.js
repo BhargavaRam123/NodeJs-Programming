a9d2ad1a1ec02dec7daba97dfca89c27c409d4fe
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-11 16:51:53 +0530

    Implement multiple condition branching with else if

--- day3/conditional branching/main.js
+++ day3/conditional branching/main.js
@@ -6,3 +6,19 @@ if (score >= 70) {
 } else {
     console.log("Failed");
 }
+
+
+
+let grade = 85;
+
+if (grade >= 90) {
+    console.log("A");
+} else if (grade >= 80) {
+    console.log("B");
+} else if (grade >= 70) {
+    console.log("C");
+} else if (grade >= 60) {
+    console.log("D");
+} else {
+    console.log("F");
+}
