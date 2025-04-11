2f56ad1e1ad3a5b6d7c6471f41daaa46443e549f
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-11 16:53:38 +0530

    Added switch case statement for multiple value checking

--- day3/conditional branching/main.js
+++ day3/conditional branching/main.js
@@ -39,3 +39,17 @@ if (username === "admin" && password === "secure123") {
 } else {
     console.log("Access denied");
 }
+
+
+let day = 3;
+let dayName;
+
+switch (day) {
+    case 1: dayName = "Monday"; break;
+    case 2: dayName = "Tuesday"; break;
+    case 3: dayName = "Wednesday"; break;
+    case 4: dayName = "Thursday"; break;
+    case 5: dayName = "Friday"; break;
+    default: dayName = "Weekend";
+}
+console.log(dayName);
