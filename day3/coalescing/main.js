e60fada498b8ffa8ccefd67a9bc1fbc3d2172903
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-11 17:05:37 +0530

    Implemented logical AND operator example

--- day3/coalescing/main.js
+++ day3/coalescing/main.js
@@ -3,3 +3,9 @@ let userName = "";
 let defaultName = "Guest";
 let displayName = userName || defaultName;
 console.log(displayName); // "Guest" (because empty string is falsy)
+
+
+let isLoggedIn = true;
+let hasPermission = true;
+let canAccess = isLoggedIn && hasPermission;
+console.log(canAccess); // true (only if both are true)
