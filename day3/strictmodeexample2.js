4557b3e9c01323224aa587e8e7c1f3f52cb7e382
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-11 12:21:50 +0530

    assignment to getter property

--- day3/strictmodeexample2.js
+++ day3/strictmodeexample2.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 Number = "hello";
 
 console.log(Number);
@@ -10,3 +10,11 @@ const obj = {}
 Object.defineProperty(obj,"x",{value:10,writable:false,enumerable:true})
 obj.x = 20
 console.log(obj)
+
+//assignment to getter property only
+const o = {
+    get func(){
+        return 10
+    }
+}
+o.func = 12
