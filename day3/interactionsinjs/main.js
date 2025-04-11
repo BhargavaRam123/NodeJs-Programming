ae1c7f512e873f9f5bb365bd12d9ac424c15514f
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-11 16:59:16 +0530

    Implement prompt to collect user input

--- day3/interactionsinjs/main.js
+++ day3/interactionsinjs/main.js
@@ -1,2 +1,10 @@
 
 alert("Welcome to our website!");
+
+
+let userName = prompt("Please enter your name:", "Guest");
+if (userName) {
+    console.log("Hello, " + userName + "!");
+} else {
+    console.log("No name provided.");
+}
