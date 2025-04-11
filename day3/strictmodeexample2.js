91f298e155aab8ea96c5a8353d092f08dc8790fd
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-11 12:16:52 +0530

    assignment to non writable property

--- day3/strictmodeexample2.js
+++ day3/strictmodeexample2.js
@@ -2,3 +2,11 @@
 Number = "hello";
 
 console.log(Number);
+
+
+//assignment to non writable property
+
+const obj = {}
+Object.defineProperty(obj,"x",{value:10,writable:false,enumerable:true})
+obj.x = 20
+console.log(obj)
