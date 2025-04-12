7f453b45ddbd57797f02410b668f5efde508ad82
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-12 10:28:34 +0530

    using arguments

--- day4/settimeout.js
+++ day4/settimeout.js
@@ -3,3 +3,11 @@ const func = ()=>{
 }
 
 setTimeout(func,2000)
+
+
+//passing settimout with arguments 
+const func2=(phrase,name)=>{
+    console.log(phrase,name)
+}
+
+setTimeout(func2,1000,"hello","john")
