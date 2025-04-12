e029066ec3026de237afd39c6cb4b3b67668d389
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-12 15:29:13 +0530

    closure example 2

--- day4/closure.js
+++ day4/closure.js
@@ -10,3 +10,12 @@ function numberGenerator() {
   
   var number = numberGenerator();
   number(); // 2
+
+  function sayHello() {
+    var say = function() { console.log(hello); }
+    // Local variable that ends up within the closure 
+    var hello = 'Hello, world!';
+    return say;
+  }
+  var sayHelloClosure = sayHello(); 
+  sayHelloClosure(); // ‘Hello, world!’
