713fddc6e719a007b3e3b62e0a6010e7600a4396
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-12 11:42:06 +0530

    realtime example of settimeout using server example

--- day4/nestedtimeout.js
+++ day4/nestedtimeout.js
@@ -6,3 +6,17 @@ let timerId = setTimeout(function tick() {
     timerId = setTimeout(tick, 2000); // (*)
     console.log("hello world")
    }, 2000);
+
+
+   let delay = 5000;
+
+// let timerId = setTimeout(function request() {
+//  //send request
+
+//  if (request failed due to server overload) {
+//    // increase the interval to the next run
+//    delay *= 2;
+//  }
+
+//  timerId = setTimeout(request, delay);
+// }, delay);
