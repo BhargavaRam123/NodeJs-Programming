303a3f1613d498c00af7cbdb1c4c5b496d045451
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-12 10:45:18 +0530

    use clearinterval to stop the timer of setinterval

--- day4/setInterval.js
+++ day4/setInterval.js
@@ -8,3 +8,8 @@ const func = ()=>{
 
 let timerid = setInterval(func,1000)
 
+//how to stop this after 5 second, here is the implementation
+setTimeout(() => {
+    clearInterval(timerid)
+    
+}, 5010);
