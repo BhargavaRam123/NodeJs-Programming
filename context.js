52b65ed6fc9752ae41837bfc46e8a0d067aa32d9
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-17 16:42:45 +0530

    Add initial implementation of context binding with call method

---
+++ context.js
@@ -0,0 +1,8 @@
+function say(phrase) {
+    alert(this.name + ': ' + phrase);
+  }
+  
+  let user = { name: "John" };
+  
+  // user becomes this, and "Hello" becomes the first argument
+  say.call( user, "Hello" ); // John: Hello
