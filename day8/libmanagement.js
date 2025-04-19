348df5c0a6984c0c37db1be247a0600e9c7316c3
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-04-19 14:53:12 +0530

    creating library management system in js

---
+++ day8/libmanagement.js
@@ -0,0 +1,19 @@
+class Book{
+    constructor(name,author,pages){
+        this.name = name
+        this.author = author
+        this.pages = pages
+    }
+
+}
+
+class Member{
+    #borrowedbooks
+    constructor(name,age,validity)
+    {
+        this.name = name
+        this.age = age
+        this.validity = validity
+    }
+
+}
